0764e9f986b9e472a447a8dfda385f919106372f
KhogaEslam <KhogaEslam@gmail.com>
2017-12-04 11:33:40 +0200

    clearing every thing

--- app/helpers/articles_helper.rb
+++
@@ -1,5 +0,0 @@
-module ArticlesHelper
-  def authorized?(article)
-    current_user == article.user
-  end
-end
--- app/models/article.rb
+++
@@ -1,8 +0,0 @@
-class Article < ApplicationRecord
-  acts_as_favoritable
-  belongs_to :user
-  #This validates presence of title, and makes sure that the length is not more than 140 words
-  validates :title, presence: true, length: {maximum: 140}
-  #This validates presence of body
-  validates :body, presence: true
-end
